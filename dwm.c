ff60f9418c4701bd13210639d14b68d28415d855
zetatez <zetatez@icloud.com>
2022-05-27 10:01:13 +0800

    cmt: patch: dwm-hide_vacant_tags: only adopt not draw rect part

--- dwm.c
+++ dwm.c
@@ -766,10 +766,10 @@ drawbar(Monitor *m)
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
-				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
-				urg & 1 << i);
+//      if (occ & 1 << i)                                                 // dwm-hide_vacant_tags: do not draw rect
+//          drw_rect(drw, x + boxs, boxs, boxw, boxw,                     // dwm-hide_vacant_tags: do not draw rect
+//              m == selmon && selmon->sel && selmon->sel->tags & 1 << i, // dwm-hide_vacant_tags: do not draw rect
+//              urg & 1 << i);                                            // dwm-hide_vacant_tags: do not draw rect
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
