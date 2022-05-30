a57a8055d20c67cdcbf610c9855bcb279d6ac1b7
zetatez <zetatez@icloud.com>
2022-05-30 21:41:19 +0800

    cmt: patch: dwm-shiftview

--- config.def.h
+++ config.def.h
@@ -116,61 +116,64 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
+#include "shiftview.c"
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = -0.025} },
-	{ MODKEY|ShiftMask,             XK_l,      setmfact,       {.f = +0.025} },
-	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_n,      setlayout,      {.v = &layouts[0]} },  // cake
-	{ MODKEY,                       XK_e,      setlayout,      {.v = &layouts[1]} },  // bstack      dwm-bottomstack
-	{ MODKEY|ShiftMask,             XK_e,      setlayout,      {.v = &layouts[2]} },  // bstackhoriz dwm-bottomstack
-    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[3]} },  // lefttile    dwm-lefttile
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[4]} },  // no layout function means floating behavior
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[5]} },  // monocle
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[6]} },  // tile
-	{ MODKEY,                       XK_w,      setlayout,      {.v = &layouts[7]} },  // tilewide    dwm-tilewide
-	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[8]} },  // sprial      dwm-fibonacci
-	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[9]} },  // dwindle     dwm-fibonacci
-    { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[10]} }, // gaplessgrid dwm-gaplessgrid
-	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[11]} }, // deck        dwm-deck-double
-	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[12]} }, // tatami      dwm-tatami
-	{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_s,      togglesticky,   {0} },                // dwm-sticky
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	/* modifier                     key            function        argument */
+	{ MODKEY,                       XK_p,          spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return,     spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,          togglebar,      {0} },
+	{ MODKEY,                       XK_j,          focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,          focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,          incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,          incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_h,          setmfact,       {.f = -0.025} },
+	{ MODKEY|ShiftMask,             XK_l,          setmfact,       {.f = +0.025} },
+	{ MODKEY|ShiftMask,             XK_j,          movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,          movestack,      {.i = -1 } },
+	{ MODKEY,                       XK_Return,     zoom,           {0} },
+	{ MODKEY,                       XK_Tab,        view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,          killclient,     {0} },
+	{ MODKEY,                       XK_n,          setlayout,      {.v = &layouts[0]} },  // cake
+	{ MODKEY,                       XK_e,          setlayout,      {.v = &layouts[1]} },  // bstack      dwm-bottomstack
+	{ MODKEY|ShiftMask,             XK_e,          setlayout,      {.v = &layouts[2]} },  // bstackhoriz dwm-bottomstack
+    { MODKEY,                       XK_t,          setlayout,      {.v = &layouts[3]} },  // lefttile    dwm-lefttile
+	{ MODKEY,                       XK_f,          setlayout,      {.v = &layouts[4]} },  // no layout function means floating behavior
+	{ MODKEY,                       XK_m,          setlayout,      {.v = &layouts[5]} },  // monocle
+	{ MODKEY|ShiftMask,             XK_t,          setlayout,      {.v = &layouts[6]} },  // tile
+	{ MODKEY,                       XK_w,          setlayout,      {.v = &layouts[7]} },  // tilewide    dwm-tilewide
+	{ MODKEY,                       XK_r,          setlayout,      {.v = &layouts[8]} },  // sprial      dwm-fibonacci
+	{ MODKEY|ShiftMask,             XK_r,          setlayout,      {.v = &layouts[9]} },  // dwindle     dwm-fibonacci
+    { MODKEY,                       XK_g,          setlayout,      {.v = &layouts[10]} }, // gaplessgrid dwm-gaplessgrid
+	{ MODKEY,                       XK_y,          setlayout,      {.v = &layouts[11]} }, // deck        dwm-deck-double
+	{ MODKEY,                       XK_o,          setlayout,      {.v = &layouts[12]} }, // tatami      dwm-tatami
+	{ MODKEY|ControlMask,		    XK_comma,      cyclelayout,    {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period,     cyclelayout,    {.i = +1 } },
+	{ MODKEY,                       XK_space,      setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,      togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_s,          togglesticky,   {0} },                // dwm-sticky
+	{ MODKEY,                       XK_0,          view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,          tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,      focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,     focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,      tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,     tagmon,         {.i = +1 } },
+    { MODKEY,                       XK_h,          shiftview,      {.i = -1 } }, // shiftview
+    { MODKEY,                       XK_l,          shiftview,      {.i = +1 } }, // shiftview
 	{ MODKEY,           			XK_apostrophe, togglescratch,  {.ui = 0 } }, // dwm-scratchpads
 	{ MODKEY,            			XK_v,	       togglescratch,  {.ui = 1 } }, // dwm-scratchpads
 	{ MODKEY,            			XK_q,	       togglescratch,  {.ui = 2 } }, // dwm-scratchpads
 	{ MODKEY,            			XK_z,	       togglescratch,  {.ui = 3 } }, // dwm-scratchpads
 	{ MODKEY,            			XK_u,	       togglescratch,  {.ui = 4 } }, // dwm-scratchpads
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	TAGKEYS(                        XK_1,          0)
+	TAGKEYS(                        XK_2,          1)
+	TAGKEYS(                        XK_3,          2)
+	TAGKEYS(                        XK_4,          3)
+	TAGKEYS(                        XK_5,          4)
+	TAGKEYS(                        XK_6,          5)
+	TAGKEYS(                        XK_7,          6)
+	TAGKEYS(                        XK_8,          7)
+	TAGKEYS(                        XK_9,          8)
+	{ MODKEY|ShiftMask,             XK_q,          quit,           {0} },
 };
 
 /* button definitions */
@@ -189,5 +192,7 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+    { ClkTagBar,            0,              Button4,        shiftview,      { .i = -1 } }, // dwm-shiftview
+    { ClkTagBar,            0,              Button5,        shiftview,      { .i = +1 } }, // dwm-shiftview
 };
 
