f1c5e9a70cfbfe5996f0d43d516d6d6ffd53914c
Hiltjo Posthuma <hiltjo@codemadness.org>
2023-04-09 12:37:14 +0200

    restore SIGCHLD sighandler to default before spawning a program
    From sigaction(2):
    A child created via fork(2) inherits a copy of its parent's signal dispositions.
    During an execve(2), the dispositions of handled signals are reset to the default;
    the dispositions of ignored signals are left unchanged.
    
    This refused to start directly some programs from configuring in config.h:
    
    static Key keys[] = {
    	MODKEY,                       XK_o,      spawn,          {.v = cmd } },
    };
    
    Some reported programs that didn't start were: mpv, anki, dmenu_extended.
    
    Reported by pfx.
    Initial patch suggestion by Storkman.

--- dwm.c
+++ dwm.c
@@ -2166,6 +2166,8 @@ sigterm(int unused)                                            // patch: dwm-res
 void
 spawn(const Arg *arg)
 {
+	struct sigaction sa;
+
 	if (arg->v == dmenucmd)
 		dmenumon[0] = '0' + selmon->num;
   selmon->tagset[selmon->seltags] &= ~scratchtag;              // patch: dwm-scratchpad
@@ -2173,6 +2175,12 @@ spawn(const Arg *arg)
 		if (dpy)
 			close(ConnectionNumber(dpy));
 		setsid();
+
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = 0;
+		sa.sa_handler = SIG_DFL;
+		sigaction(SIGCHLD, &sa, NULL);
+
 		execvp(((char **)arg->v)[0], (char **)arg->v);
 		die("dwm: execvp '%s' failed:", ((char **)arg->v)[0]);
 	}
