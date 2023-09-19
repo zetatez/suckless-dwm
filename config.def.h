a6012bcac6892f6b4a81f2f96c549bafabe94543
Dionysus <zetatez@icloud.com>
2023-09-19 10:20:24 +0800

    cmt: change font size

--- config.def.h
+++ config.def.h
@@ -9,12 +9,12 @@ static const int scalepreview       = 3;        /* preview scaling (display w an
 static const int previewbar         = 1;        /* show the bar in the preview window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int barheight          = 16;       /* bh = (barheight > drw->fonts->h ) && (barheight < 3 * drw->fonts->h ) ? barheight : drw->fonts->h + 2 */ // patch: dwm-bar-height
+static const int barheight          = 12;       /* bh = (barheight > drw->fonts->h ) && (barheight < 3 * drw->fonts->h ) ? barheight : drw->fonts->h + 2 */ // patch: dwm-bar-height
 static const int vertpad            = 0;        /* default 6  : vertical padding of bar */                                                                               // patch: dwm-barpadding
 static const int sidepad            = 0;        /* default 256: horizontal padding of bar */                                                                             // patch: dwm-barpadding
 static const int defaultwinpad      = 0;        /* default 8  : window padding of bar */
 static const int swallowfloating    = 1;        /* default 1  : 1 means swallow floating windows */                                                           // patch: dwm-swallow
-static const char *fonts[]          = {"DejaVuSansMono Nerd Font:style=Book:size=14"};
+static const char *fonts[]          = {"DejaVuSansMono Nerd Font:style=Book:size=12"};
 static const char dmenufont[]       = "DejaVuSansMono Nerd Font:style=Book:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
