5c44fedc417db6f2bb1aa893c1e1511781f8f552
MaQianyi <zetatez@icloud.com>
2024-08-07 09:48:42 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -37,7 +37,6 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
-  "xset r rate 148 128", NULL,
   "dwmblocks", NULL,
   "picom", "-b", NULL,
   "dunst", "&", NULL,
@@ -98,7 +97,7 @@ static const char *scratchpadcmd[]     = { "st", "-g", "120x32", "-t", "scratchp
 static const char *screen_light_dec[]                 = SH("sudo light -U 5");
 static const char *screen_light_inc[]                 = SH("sudo light -A 5");
 static const char *screen_lock[]                      = SH("slock");
-static const char *keyboard_rate[]                    = SH("xset r rate 148 128 && notify-send 'keyboard rate changed to 148 128'");
+static const char *keyboard_rate[]                    = SH("xset r rate 158 128 && notify-send 'keyboard rate changed to 158 128'");
 static const char *sys_suspend[]                      = SH("systemctl suspend && slock");
 static const char *sys_shutdown[]                     = SH("systemctl poweroff");
 static const char *volume_dec[]                       = SH("amixer set Speaker unmute; amixer set Speaker 98; amixer set Master 5%-");
