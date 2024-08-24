6a2aebc020ecc42f60aa3ffd0afa89a0ab29645f
MaQianyi <zetatez@icloud.com>
2024-08-24 11:56:54 +0800

    cmt

--- dwm.c
+++ dwm.c
@@ -3270,35 +3270,40 @@ layout_fibonacci(Monitor *m, int s)
   for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
     if ((i % 2 && nh / 2 > 2 * c->bw) || (!(i % 2) && nw / 2 > 2 * c->bw)) {
       if (i < n - 1) {
-        if (i % 2)
+        if (i % 2) {
           nh /= 2;
-        else
+        } else {
           nw /= 2;
-        if ((i % 4) == 2 && !s)
+        }
+        if ((i % 4) == 2 && !s) {
           nx += nw;
-        else if ((i % 4) == 3 && !s)
+        } else if ((i % 4) == 3 && !s) {
           ny += nh;
+        }
       }
 
       if ((i % 4) == 0) {
-        if (s)
+        if (s) {
           ny += nh;
-        else
+        } else {
           ny -= nh;
+        }
       } else if ((i % 4) == 1) {
         nx += nw;
       } else if ((i % 4) == 2) {
         ny += nh;
       } else if ((i % 4) == 3) {
-        if (s)
+        if (s) {
           nx += nw;
-        else
+        } else {
           nx -= nw;
+        }
       }
 
       if (i == 0) {
-        if (n != 1)
+        if (n != 1) {
           nw = m->ww * m->mfact;
+        }
         ny = m->wy;
       } else if (i == 1) {
         nw = m->ww - nw;
