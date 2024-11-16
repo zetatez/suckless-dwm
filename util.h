d37b8e341db7469781494a87ecc8a4ee69935a55
MaQianyi <zetatez@icloud.com>
2024-11-16 19:03:02 +0800

    util.c: output function might override errno and thus affect perror()
    Original patch by Raymond Cole with some modifications, thanks!

--- util.h
+++ util.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #define MAX(A, B)               ((A) > (B) ? (A) : (B))
 #define MIN(A, B)               ((A) < (B) ? (A) : (B))
