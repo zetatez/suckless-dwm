2df5847b174b0b8130c0e617003ed3197134faea
MaQianyi <zetatez@icloud.com>
2024-11-16 19:03:02 +0800

    util.c: output function might override errno and thus affect perror()
    Original patch by Raymond Cole with some modifications, thanks!

--- util.c
+++ util.c
@@ -5,17 +5,17 @@ void
 die(const char *fmt, ...)
 {
   va_list ap;
+  int saved_errno = errno;
 
   va_start(ap, fmt);
   vfprintf(stderr, fmt, ap);
   va_end(ap);
 
   if (fmt[0] && fmt[strlen(fmt)-1] == ':') {
-    fputc(' ', stderr);
-    perror(NULL);
-  } else {
-    fputc('\n', stderr);
+    fprintf(stderr, " %s", strerror(saved_errno));
   }
+
+  fputc('\n', stderr);
 
   exit(1);
 }
