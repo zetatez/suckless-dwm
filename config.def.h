535cc744ba356198cf31b3afdaaabc7626cff146
MaQianyi <zetatez@icloud.com>
2025-01-21 01:34:15 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -45,7 +45,7 @@ static const Rule rules[] = {
   /* cls                     instance    title      tags mask     isfloating    isterminal     noswallow    monitor */
   {"floatwindow",            NULL,       NULL,      0,            1,            0,             0,           -1 },
   {"st",                     NULL,       NULL,      0,            0,            1,             1,           -1 },
-  {"Vimb",                   NULL,       NULL,      0,            0,            0,             1,           -1 }, // for markdown.    Vimb V must be capital
+  {"qutebrowser",            NULL,       NULL,      0,            0,            0,             1,           -1 }, // for markdown.
   {"netease-cloud-music",    NULL,       NULL,      1<<8,         0,            0,             0,           -1 },
 };
 
