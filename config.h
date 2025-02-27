e309d2b47c4ae41f4a4515cbb46e50c547b1ad25
ShiYi <zetatez@icloud.com>
2025-02-27 21:28:27 +0800

    cmt

--- config.h
+++ config.h
@@ -57,7 +57,7 @@ static const XPoint stickyiconbb    = {4,8};	                                   
 static const float mfact            = 0.50;
 static const float hfact            = 0.50;
 static const int nmaster            = 1;
-static const int maxnmaster         = 8;
+static const int maxnmaster         = 16;
 static const int resizehints        = 0;
 static const int lockfullscreen     = 0;
 
