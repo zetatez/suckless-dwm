bca5f9a39a231059d7bc8aa5c125362ebe148e7d
ShiYi <zetatez@icloud.com>
2025-10-12 22:08:55 +0800

    cmt

--- config.h
+++ config.h
@@ -25,14 +25,14 @@ static const char col_gray2[]      = "#444444";
 static const char col_gray3[]      = "#bbbbbb";
 static const char col_gray4[]      = "#eeeeee";
 static const char col_cyan[]       = "#005577";
-static const char col_bg[]       = "#E06C75";
-static const char col_fg[]       = "#FFFF00";
+static const char col_bg[]         = "#008B8B";
+static const char col_fg[]         = "#00BFFF";
 static const char *colors[][3]     = {
-//                  fg         bg        border
-[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-   // [SchemeNorm] = { col_bg   , col_cyan, col_gray2 },
-   // [SchemeSel]  = { col_fg   , col_cyan, col_cyan  },
+//               fg         bg        border
+// [SchemeNorm] = { col_gray3, col_cyan,  col_gray2 },
+// [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+   [SchemeNorm] = { col_bg   , col_cyan, col_gray2 },
+   [SchemeSel]  = { col_fg   , col_cyan, col_cyan  },
 };
 
 static const char *const autostart[] = {
