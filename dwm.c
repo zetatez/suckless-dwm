5d7d9b880cb87c9c99de577267df4bf04be74f45
shiyi <zetatez@icloud.com>
2026-01-30 00:20:24 +0800

    patch: dwm-hide_vacant_tags-6.3

--- dwm.c
+++ dwm.c
@@ -463,14 +463,25 @@ buttonpress(XEvent *e)
     selmon = m;
     focus(NULL);
   }
-  if (ev->window == selmon->barwin) {
-    i = x = 0;
-    do {
-      x += TEXTW(tags[i]);
-    } while (ev->x >= x && ++i < LENGTH(tags));
-    if (i < LENGTH(tags)) {
-      click = ClkTagBar;
-      arg.ui = 1 << i;
+	if (ev->window == selmon->barwin) {
+		i = x = 0;
+		unsigned int occ = 0;
+    for (c = m->clients; c; c = c->next) {
+      occ |= c->tags;
+    }
+		while (i < LENGTH(tags)) {
+			/* Do not reserve space for vacant tags */
+			if (occ & 1 << i || m->tagset[m->seltags] & 1 << i) {
+				x += TEXTW(tags[i]);
+        if (ev->x < x) {
+          break;
+        }
+			}
+			i++;
+		}
+		if (i < LENGTH(tags)) {
+			click = ClkTagBar;
+			arg.ui = 1 << i;
       if (selmon->previewshow) {
         selmon->previewshow = 0;
         XUnmapWindow(dpy, selmon->tagwin);
@@ -834,17 +845,17 @@ drawbar(Monitor *m)
   x = 0;
   if (m->isoverview) {
     //
-  } else {
-    for (i = 0; i < LENGTH(tags); i++) {
-      w = TEXTW(tags[i]);
-      drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
-      drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-      if (occ & 1 << i) {
-        drw_rect(drw, x + boxs, boxs, boxw, boxw, m == selmon && selmon->sel && selmon->sel->tags & 1 << i, urg & 1 << i);
-      }
-      x += w;
-    }
-  }
+	} else {
+		for (i = 0; i < LENGTH(tags); i++) {
+			/* Do not draw vacant tags */
+			if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+				continue;
+			w = TEXTW(tags[i]);
+			drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+			drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
+			x += w;
+		}
+	}
   w = TEXTW(m->ltsymbol);
   drw_setscheme(drw, scheme[SchemeNorm]);
   x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
